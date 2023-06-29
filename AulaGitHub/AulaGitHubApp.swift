a53f1d8dfb2975bf351cdbb675fdd59c428781ee
Rafael Carreira <carreirarafael3@gmail.com>
2023-06-29 16:31:58 -0300

    Initial Commit

---
+++ AulaGitHub/AulaGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  AulaGitHubApp.swift
+//  AulaGitHub
+//
+//  Created by Rafael Carreira on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AulaGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
